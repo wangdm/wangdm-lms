09d4a6e16fdb54281421988abe1234ee39f21172
wangdm <wangdm1989@gmail.com>
2016-09-03 12:20:47 +0800

    1.persistence-mysql.properties 更名为 datasource.properties 2.去掉AdminInterceptor未实现函数和打印

--- wangdm-lms-admin/src/main/java/com/wangdm/lms/admin/interceptor/AdminInterceptor.java
+++ wangdm-lms-admin/src/main/java/com/wangdm/lms/admin/interceptor/AdminInterceptor.java
@@ -3,42 +3,16 @@ package com.wangdm.lms.admin.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class AdminInterceptor extends HandlerInterceptorAdapter {
-    
-    private static Logger log = LoggerFactory.getLogger(AdminInterceptor.class);
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
-        log.debug("AdminInterceptor.preHandle");
-        return super.preHandle(request, response, handler);
-    }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
-        log.debug("AdminInterceptor.postHandle");
         request.setAttribute("contextPath", request.getContextPath());
         super.postHandle(request, response, handler, modelAndView);
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-            throws Exception {
-        log.debug("AdminInterceptor.afterCompletion");
-        super.afterCompletion(request, response, handler, ex);
-    }
-
-    @Override
-    public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
-        log.debug("AdminInterceptor.afterConcurrentHandlingStarted");
-        super.afterConcurrentHandlingStarted(request, response, handler);
     }
 
 }
